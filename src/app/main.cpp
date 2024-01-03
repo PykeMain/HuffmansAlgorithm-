35a0ee60838fad8f323381d937461d9372647b3a
Bemba <bugybon1@gmail.com>
2024-01-03 02:22:50 +0200

    pragma once is added

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,16 +3,17 @@
 #include "../structures/include/binaryTree.hpp"
 #include <iostream>
 #include <bitset>
+#include <fstream>
 
 int main(){
-    std::vector<std::pair<char,std::size_t>> test;
+    std::string name = "wibawoba.txt";
+    std::string content;
+    std::ifstream in("../../text" + name);
+    std::vector<std::pair<char,std::size_t>> test = letterCounter("this is an example of a huffman tree");
     std::vector<std::pair<std::string, bool>> lookUpTable(256, std::make_pair("", false));
     std::vector<std::pair<std::string, bool>> lookUpTable2(256, std::make_pair("", false));
-    std::string table = "";
-    test = letterCounter("this is an example of a huffman tree");
     printHuffmanTree(createHuffmanTree(test), 0, 0, lookUpTable);
 
-    std::string temp;
     for(int i = 0; i < 256; ++i){
         if(lookUpTable[i].second){
             std::cout << (char) i << " " << lookUpTable[i].first << std::endl;
