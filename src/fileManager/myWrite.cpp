f5fec8de3c94fdd6b634d8cc2a71999a8da2de62
Bemba <bugybon1@gmail.com>
2024-01-07 03:17:48 +0200

    eepy

--- src/fileManager/myWrite.cpp
+++ src/fileManager/myWrite.cpp
@@ -18,21 +18,21 @@ void writeASCII(std::ofstream& out, std::string content){
 
 void writeBinary(std::ofstream& out, std::string content){
     unsigned int temp;
-    for(int i = 0; i < content.size() / 32; ++i){
+    for(int i = 0; i < content.size() / 16; ++i){
         temp = 0;
-        for(int j = 0; j < 32; ++j){
-            temp = (temp << 1) + ((content[i * 32 + j] == '0') ? 0 : 1);
-            //std::cout << (content[i * 32 + j] == '0') ? 0 : 1;
+        for(int j = 0; j < 16; ++j){
+            temp = (temp << 1) + ((content[i * 16 + j] == '0') ? 0 : 1);
+            //std::cout << (content[i * 16 + j] == '0') ? 0 : 1;
         }
-        out.write((const char*)&temp, 4);
+        out.write((const char*)&temp, sizeof(int));
         std::cout << "=" << temp << " ";
     }
-    if(content.size() % 32 != 0){
+    if(content.size() % 16 != 0){
         temp = 0;
-        for(int k = 0; k < content.size() % 32 ; ++k){
-            temp = (temp << 1) + ((content[content.size() - content.size() % 32 + k] == '0') ? 0 : 1);
+        for(int k = 0; k < content.size() % 16 ; ++k){
+            temp = (temp << 1) + ((content[content.size() - content.size() % 16 + k] == '0') ? 0 : 1);
         }
-        out.write((const char*)&temp,4);
+        out.write((const char*)&temp, sizeof(int));
     }
     // command to read the binary: xxd -b ../../text/test2_wibo.dat !!
 }
