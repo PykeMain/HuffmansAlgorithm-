8149c9fb59e13cbf6d15a8790868794958c75cce
Bemba <bugybon1@gmail.com>
2024-01-09 21:03:39 +0200

    Documentation for algorithms is ready

--- src/algorithms/include/generateHuffmanTree.hpp
+++ src/algorithms/include/generateHuffmanTree.hpp
@@ -8,15 +8,37 @@
 #include "../../structures/include/binaryTree.hpp"
 #include "../../structures/include/linkedQueue.hpp"
 
+//! Chooses the smallest two trees from both queue.
+//! @param first A queue made from a linked list consisting entirely of leafs
+//! @param second A queue made from a linked list consisting of nonleafs
+//! @return The two trees with the 
+binaryTree* pickTree(LinkedQueue<binaryTree*>& first, LinkedQueue<binaryTree*>& second);
 
-binaryTree* pickTree(std::queue<binaryTree*>& first, std::queue<binaryTree*>& second);
-
+//! Generates a Huffman tree. By getting the 2 smallest trees it combines then into a new tree with a root their combined occurrence. It stops when there is only 1 element left in both, which is the Huffman tree.
+//! @param occurrenceTable Created from letterCounter.cpp and tells how often each symbol occurs.
+//! @return The Huffman tree.
 binaryTree* createHuffmanTree(std::vector<myPair> occurrenceTable);
 
-void printHuffmanTree(binaryTree*, std::size_t, std::size_t, std::vector<std::pair<std::string, bool>>&);
+//! Generates recursively the encoded values for every used symbol from the Huffman tree. Going left gives a 1, while going right - 0.
+//! @param root The Huffman tree
+//! @param code Current binary encoding
+//! @param codeSize Used for remembering the leading zeros
+//! @param lookUpTable Vector remembering which symbol is used and what is the given code.
+void printHuffmanTree(binaryTree* root, std::size_t code, std::size_t codeSize, std::vector<std::pair<std::string, bool>>& lookUpTable);
 
-std::string leafToBinary(std::size_t, std::size_t);
+//! From a given code and codeSize it returns the binary in string format.
+//! @param code Binary encoding
+//! @param codeSize Used for remembering the leading zeros
+//! @return Encoded binary representation of the symbol
+std::string leafToBinary(std::size_t code, std::size_t codeSize);
 
-std::string byteToNumber(std::string);
+//! From an exact binary sequence it creates the decimal number it represents.
+//! @param encoded Binary sequence
+//! @return The decimal number behind the binary sequence
+std::string byteToNumber(std::string encoded);
 
-std::string decodeHuffman(binaryTree*, std::string);
+//! Decodes the text from a given tree. The function goes down the tree and when it reaches a leaf it records the symbol connected to it.
+//! @param encoded Binary sequence
+//! @param root The Huffman tree
+//! @return The decoded version of the text
+std::string decodeHuffman(binaryTree* root, std::string encoded);
--- src/algorithms/include/letterCounter.hpp
+++ src/algorithms/include/letterCounter.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
+/** @file letterCounter.hpp
+ *  @brief Implementation of a letter counter in a given text and a sort it requires 
+ *  @author Momchil Mladenov, 5MI0800232, Group 3
+*/
+
 #include <vector>
 #include <string>
 #include <cstddef>
 #include <iostream>
 
+
 typedef std::pair<char,std::size_t> myPair;
 
-void heapify(std::vector<myPair>&, const std::size_t&, const std::size_t& );
+//! Treats the array as a tree and swaps the children if they have larger values than their partent.
+//! @param array Array of pairs of chars and std::size_t
+//! @param sizeTree The max amount a child can be. If larger then that then it's ignored
+//! @param root The parent index
+void heapify(std::vector<myPair>& array,const std::size_t& sizeTree, const std::size_t& root);
 
-void heapSort(std::vector<myPair>&);
+//! Starts a heap sort using the heapify function
+//! @param array Array of pairs of chars and std::size_t
+void heapSort(std::vector<myPair>& array);
 
-std::vector<myPair> letterCounter(std::string);
+//! Counts the letters in a given text one after another. Sorts the used letters after the counting has finished.
+//! @param input Text
+//! @return Sorted array of letters and the times each has occured
+std::vector<myPair> letterCounter(std::string input);
